d8bdd5b20531204aeee172ce6a19a65bfb6627d2
Sina-Taghizadeh <sina.taghizadeh123@gmail.com>
2024-03-13 19:28:52 +0330

    Updated comments and added helper files

--- FEM_code.m
+++ FEM_code.m
@@ -1,17 +1,20 @@
 % A general MATLAB code to solve 2D problems with truss, triangle and bilinear quadrilateral elements
 % by Sina Taghizadeh
 % July 2022
+author = "Sina Taghizadeh";
+license = "GNU GPL Version 3.0";
 
 clc;clear;close all
 
 %----------------------------------------------------------------------
-% Get inputs from user (Excel) or mesh generation if needed
+% User Input or Automatic Mesh Generation for Triangle Elements
 %----------------------------------------------------------------------
 
 % insert the excel file name
-excelfile='input_sample.xlsx';
+Data='input_sample_truss.xlsx';
+excelfile=Data;
 
-% x,y of each node in global cordinate system
+% X,y of each node in global cordinate system
 Nodes_xy=xlsread(excelfile,'k2:oo3')'; 
 
 % each row is these informations of each element: 
@@ -19,48 +22,48 @@ Nodes_xy=xlsread(excelfile,'k2:oo3')';
 elements=xlsread(excelfile,'b2:e1000');
 
 % other properties of each element
-Atrusselements=xlsread(excelfile,'f2:f1000');
-Eandv=xlsread(excelfile,'h2:i1000');
-telements=xlsread(excelfile,'g2:g1000');
+TrussElementsArea=xlsread(excelfile,'f2:f1000');
+EandNu=xlsread(excelfile,'h2:i1000');
+ElementsThickness=xlsread(excelfile,'g2:g1000');
 
 % Given all external forces 
 Given_Force=xlsread(excelfile,'k4:oo5');
 
-% Given Boundary Conditions = 0
+% Given Fixed Boundary Conditions 
 zero_dof=xlsread(excelfile,'k6:oo7');  
 
-% Automatic mesh generation just for triangle elements
+% Automatic mesh generation just for triangle elements and rectangular domains if needed
 generation=0;   % should fill by user (1 for on & 0 for off) 
 if generation
-    tool=9;   %toole mostatil
-    arz=1;    %arz mostatil
-    t_tool=20;   %should fill by user
-    t_arz=2;     %should fill by user
-    Nodes_gen=zeros((t_tool+1)*(t_arz/2+1),2);  %preallocation
-    elem_gen=zeros(t_tool*t_arz,3);             %preallocation
-    a=t_arz/2+1;     %Auxiliary variable
+    RectanguLarlength=9;   %The length of the rectangular domain
+    RectanguLarWidth=1;    %The width of the rectangular domain
+    n_length=20;   %should fill by user
+    n_width=8;     %should fill by user
+    Nodes_gen=zeros((n_length+1)*(n_width/2+1),2);  %preallocation
+    elem_gen=zeros(n_length*n_width,3);             %preallocation
+    a=n_width/2+1;     %Auxiliary variable
     b=0;             %Counter
-    for i=1:a:size(Nodes_gen)      %for x coordinate of nodes
-        Nodes_gen(i,1)=b*tool/t_tool;
+    for i=1:a:size(Nodes_gen)      %for X coordinate of nodes
+        Nodes_gen(i,1)=b*RectanguLarlength/n_length;
         Nodes_gen(i+1:i+a-1,1)=Nodes_gen(i,1);
         b=b+1;
     end
     b=0;
     for i=1:a                      %for y coordinate of nodes
-        Nodes_gen(i,2)=2*b*arz/t_arz;
+        Nodes_gen(i,2)=2*b*RectanguLarWidth/n_width;
         b=b+1;
-        for o=1:t_tool
+        for o=1:n_length
             Nodes_gen(i+o*a,2)=Nodes_gen(i,2);
         end
     end
     b=1;
-    for i=1:2:t_arz*t_tool         %for node connectivity of elements
+    for i=1:2:n_width*n_length         %for node connectivity of elements
         elem_gen(i:i+1,1)=b;
         elem_gen(i+1,3)=elem_gen(i+1,1)+1;
         elem_gen(i+1,2)=b+a+1;
         elem_gen(i,2)=elem_gen(i+1,1)+a;
         elem_gen(i,3)=b+a+1;
-        if mod(b,a)==t_arz/2
+        if mod(b,a)==n_width/2
             b= b+1;
         end
         b=b+1;
@@ -69,31 +72,34 @@ if generation
     elements=elem_gen;
     
     % Change the rest of the properties of the Excel file
-    Eandv(3:t_tool*t_arz,1)=Eandv(1,1);
-    Eandv(3:t_tool*t_arz,2)=Eandv(1,2);
-    telements(3:t_tool*t_arz)=telements(1);
-    Given_Force(:,1:(t_tool+1)*(t_arz/2+1))=0;
-    Given_Force(1,(t_tool+1)*(t_arz/2+1))=+100000;
-    Given_Force(1,(t_tool+1)*(t_arz/2+1)-t_arz/2)=-100000;
+    EandNu(3:n_length*n_width,1)=EandNu(1,1);
+    EandNu(3:n_length*n_width,2)=EandNu(1,2);
+    ElementsThickness(3:n_length*n_width)=ElementsThickness(1);
+    Given_Force(:,1:(n_length+1)*(n_width/2+1))=0;
+    Given_Force(1,(n_length+1)*(n_width/2+1))=+100000;
+    Given_Force(1,(n_length+1)*(n_width/2+1)-n_width/2)=-100000;
     zero_dof(:,1:a)=1;
-    zero_dof(:,a+1:(t_tool+1)*(t_arz/2+1))=0;
+    zero_dof(:,a+1:(n_length+1)*(n_width/2+1))=0;
 end
 %--------------------------------------------------------------------------
-% End of Get inputs from user (Excel) or mesh generation if needed
+% End of User Input or Automatic Mesh Generation for Triangle Elements
 %--------------------------------------------------------------------------
 
 
 %----------------------------------------------------------------------
-% Production of problem prerequisites
+% Production of Problem Prerequisites
 %----------------------------------------------------------------------
 
 tic;   %for runtime of my program
 
+null = 0.000001; %define zero for problem
+ScalingFactor=100; %for Visualization of results
+
 % Number of nodes in the problem
-no_nodes=size(Nodes_xy,1);
+n_nodes=size(Nodes_xy,1);
 
 % total degree of fredom (for planar problems)
-tot_dof=no_nodes * 2;
+tot_dof=n_nodes * 2;
 
 % number of elements
 no_elements = size(elements,1);
@@ -105,15 +111,13 @@ Force=zeros(no_elements,1);       % stress vector
 
 % P = matrix of each node cordinate numbers in Global
 P=reshape(1:tot_dof,2,[])';
-
-
 %--------------------------------------------------------------------------
-% End of Production of problem prerequisites
+% End of Production of Problem Prerequisites
 %--------------------------------------------------------------------------
 
 
 %----------------------------------------------------------------------
-% Create and assembly of all stiffness matrices
+% Stiffness Matrix Generation and Assembly
 %----------------------------------------------------------------------
 
 % creat stiffnes matrices and assembly for 2D elements
@@ -123,7 +127,7 @@ for     j=1:no_elements    %loop for creating k element
         L= ((Nodes_xy(elements(j,2),1) - Nodes_xy(elements(j,1),1))^2 + (Nodes_xy(elements(j,2),2) - Nodes_xy(elements(j,1),2))^2  )^0.5 ;
         c=(Nodes_xy(elements(j,2),1) - Nodes_xy(elements(j,1),1))/L;         %cos    
         s=(Nodes_xy(elements(j,2),2) - Nodes_xy(elements(j,1),2))/L;         %sin     
-        Kelement=(Atrusselements(j)*Eandv(j)/L)*[c*c c*s -c*c -c*s;c*s s*s -c*s -s*s;-c*c -c*s c*c c*s;-c*s -s*s c*s s*s]; % k for each element
+        Kelement=(TrussElementsArea(j)*EandNu(j)/L)*[c*c c*s -c*c -c*s;c*s s*s -c*s -s*s;-c*c -c*s c*c c*s;-c*s -s*s c*s s*s]; % k for each element
         stiffness([P(elements(j,1),:) P(elements(j,2),:)],[P(elements(j,1),:) P(elements(j,2),:)])= stiffness([P(elements(j,1),:) P(elements(j,2),:)],[P(elements(j,1),:) P(elements(j,2),:)]) +  Kelement; %assembly
     end
     
@@ -132,9 +136,9 @@ for     j=1:no_elements    %loop for creating k element
         Belement=(1/(2*delta))*[Nodes_xy(elements(j,2),2)-Nodes_xy(elements(j,3),2) 0 Nodes_xy(elements(j,3),2)-Nodes_xy(elements(j,1),2) 0 Nodes_xy(elements(j,1),2)-Nodes_xy(elements(j,2),2) 0 ;
                                 0 Nodes_xy(elements(j,3),1)-Nodes_xy(elements(j,2),1) 0 Nodes_xy(elements(j,1),1)-Nodes_xy(elements(j,3),1) 0 Nodes_xy(elements(j,2),1)-Nodes_xy(elements(j,1),1) ;
                                 Nodes_xy(elements(j,3),1)-Nodes_xy(elements(j,2),1) Nodes_xy(elements(j,2),2)-Nodes_xy(elements(j,3),2) Nodes_xy(elements(j,1),1)-Nodes_xy(elements(j,3),1) Nodes_xy(elements(j,3),2)-Nodes_xy(elements(j,1),2) Nodes_xy(elements(j,2),1)-Nodes_xy(elements(j,1),1) Nodes_xy(elements(j,1),2)-Nodes_xy(elements(j,2),2)];
-        C=(Eandv(j,1)/(1-(Eandv(j,2))^2))*[1 Eandv(j,2) 0; Eandv(j,2) 1 0; 0 0 (1-Eandv(j,2))/2]; %for plane stress
-        %C=(Eandv(j,1)/((1+(Eandv(j,2)))*(1-2*(Eandv(j,2)))))*[1-Eandv(j,2) Eandv(j,2) 0; Eandv(j,2) 1-Eandv(j,2) 0; 0 0 (1-2*(Eandv(j,2)))/2]; %for plane strain
-        Kelement=delta*telements(j)*transpose(Belement)*C*Belement;
+        C=(EandNu(j,1)/(1-(EandNu(j,2))^2))*[1 EandNu(j,2) 0; EandNu(j,2) 1 0; 0 0 (1-EandNu(j,2))/2]; %for plane stress
+        %C=(EandNu(j,1)/((1+(EandNu(j,2)))*(1-2*(EandNu(j,2)))))*[1-EandNu(j,2) EandNu(j,2) 0; EandNu(j,2) 1-EandNu(j,2) 0; 0 0 (1-2*(EandNu(j,2)))/2]; %for plane strain
+        Kelement=delta*ElementsThickness(j)*transpose(Belement)*C*Belement;
         stiffness([P(elements(j,1),:) P(elements(j,2),:) P(elements(j,3),:)],[P(elements(j,1),:) P(elements(j,2),:) P(elements(j,3),:)])= stiffness([P(elements(j,1),:) P(elements(j,2),:) P(elements(j,3),:)],[P(elements(j,1),:) P(elements(j,2),:) P(elements(j,3),:)]) +  Kelement; %assembly
     end 
     
@@ -153,7 +157,8 @@ for     j=1:no_elements    %loop for creating k element
             bder(o+4)=a(2);
         end
         Belement=[bder(1) 0 bder(2) 0 bder(3) 0 bder(4) 0; 0 bder(5) 0 bder(6) 0 bder(7) 0 bder(8); bder(5) bder(1) bder(6) bder(2) bder(7) bder(3) bder(8) bder(4) ];
-        C=(Eandv(j,1)/(1-(Eandv(j,2))^2))*[1 Eandv(j,2) 0; Eandv(j,2) 1 0; 0 0 (1-Eandv(j,2))/2];
+        C=(EandNu(j,1)/(1-(EandNu(j,2))^2))*[1 EandNu(j,2) 0; EandNu(j,2) 1 0; 0 0 (1-EandNu(j,2))/2]; %for plane stress
+        %C=(EandNu(j,1)/((1+(EandNu(j,2)))*(1-2*(EandNu(j,2)))))*[1-EandNu(j,2) EandNu(j,2) 0; EandNu(j,2) 1-EandNu(j,2) 0; 0 0 (1-2*(EandNu(j,2)))/2]; %for plane strain
         Kelement=0;
         for t=1:4
             gauss=[0.57735 0.57735;-0.57735 0.57735;0.57735 -0.57735;-0.57735 -0.57735];
@@ -165,9 +170,8 @@ for     j=1:no_elements    %loop for creating k element
         stiffness([P(elements(j,1),:) P(elements(j,2),:) P(elements(j,3),:) P(elements(j,4),:)],[P(elements(j,1),:) P(elements(j,2),:) P(elements(j,3),:) P(elements(j,4),:)])= stiffness([P(elements(j,1),:) P(elements(j,2),:) P(elements(j,3),:) P(elements(j,4),:)],[P(elements(j,1),:) P(elements(j,2),:) P(elements(j,3),:) P(elements(j,4),:)]) +  Kelement; %assembly
     end
 end
-
 %--------------------------------------------------------------------------
-% End of Create and assembly of all stiffness matrices
+% End of Stiffness Matrix Generation and Assembly
 %--------------------------------------------------------------------------
 
 
@@ -183,14 +187,13 @@ unknown_dofNodes= setdiff((1:tot_dof)',Given_dof');
 
 % apply given external forces to force vector
 Forces = Given_Force(:)  ;
-
 %--------------------------------------------------------------------------
 % End of Apply BCs
 %--------------------------------------------------------------------------
 
 
 %----------------------------------------------------------------------
-% Solving with LU decomposition
+% Solve Linear System with LU Factorization Decomposition
 %----------------------------------------------------------------------
 
 % determination of unkhown displacements with LU decomposition
@@ -198,19 +201,18 @@ Forces = Given_Force(:)  ;
 y = Li\(qi*Forces(unknown_dofNodes));
 displacements(unknown_dofNodes) = Ui\y;
 u=displacements;
-
 %--------------------------------------------------------------------------
-% End of Solving with LU decomposition
+% End of Solve Linear System with LU Factorization Decomposition
 %--------------------------------------------------------------------------
 
 
 %----------------------------------------------------------------------
-% post processing and showing outputs
+% Post-Processing and Visualization of Results
 %----------------------------------------------------------------------
 
 % determination of nodal forces
 F= stiffness * displacements;
-F(abs(F)<0.00001)=0;
+F(abs(F)<null)=0;
 
 % determination of Forces and stresse of each elements for 2D
 if size(elements,2) == 2  %truss element
@@ -218,8 +220,8 @@ if size(elements,2) == 2  %truss element
     L= ((Nodes_xy(elements(j,2),1) - Nodes_xy(elements(j,1),1))^2 + (Nodes_xy(elements(j,2),2) - Nodes_xy(elements(j,1),2))^2  )^0.5 ;
     c=(Nodes_xy(elements(j,2),1) - Nodes_xy(elements(j,1),1))/L; %cx
     s=(Nodes_xy(elements(j,2),2) - Nodes_xy(elements(j,1),2))/L; %cy
-    Force(j)=(Eandv(j)/L)*[-c -s c s]*u([P(elements(j,1),:) P(elements(j,2),:)])*Atrusselements(j);
-    Force(abs(Force)<0.00001)=0;
+    Force(j)=(EandNu(j)/L)*[-c -s c s]*u([P(elements(j,1),:) P(elements(j,2),:)])*TrussElementsArea(j);
+    Force(abs(Force)<null)=0;
     end
 end
 if size(elements,2) == 3  %triangle elements
@@ -229,10 +231,10 @@ if size(elements,2) == 3  %triangle elements
     Belement=(1/(2*delta))*[Nodes_xy(elements(j,2),2)-Nodes_xy(elements(j,3),2) 0 Nodes_xy(elements(j,3),2)-Nodes_xy(elements(j,1),2) 0 Nodes_xy(elements(j,1),2)-Nodes_xy(elements(j,2),2) 0 ;
                                 0 Nodes_xy(elements(j,3),1)-Nodes_xy(elements(j,2),1) 0 Nodes_xy(elements(j,1),1)-Nodes_xy(elements(j,3),1) 0 Nodes_xy(elements(j,2),1)-Nodes_xy(elements(j,1),1) ;
                                 Nodes_xy(elements(j,3),1)-Nodes_xy(elements(j,2),1) Nodes_xy(elements(j,2),2)-Nodes_xy(elements(j,3),2) Nodes_xy(elements(j,1),1)-Nodes_xy(elements(j,3),1) Nodes_xy(elements(j,3),2)-Nodes_xy(elements(j,1),2) Nodes_xy(elements(j,2),1)-Nodes_xy(elements(j,1),1) Nodes_xy(elements(j,1),2)-Nodes_xy(elements(j,2),2)];
-    C=(Eandv(j,1)/(1-(Eandv(j,2))^2))*[1 Eandv(j,2) 0; Eandv(j,2) 1 0; 0 0 (1-Eandv(j,2))/2];
-    %C=(Eandv(j,1)/((1+(Eandv(j,2)))*(1-2*(Eandv(j,2)))))*[1-Eandv(j,2) Eandv(j,2) 0; Eandv(j,2) 1-Eandv(j,2) 0; 0 0 (1-2*(Eandv(j,2)))/2];
+    C=(EandNu(j,1)/(1-(EandNu(j,2))^2))*[1 EandNu(j,2) 0; EandNu(j,2) 1 0; 0 0 (1-EandNu(j,2))/2];
+    %C=(EandNu(j,1)/((1+(EandNu(j,2)))*(1-2*(EandNu(j,2)))))*[1-EandNu(j,2) EandNu(j,2) 0; EandNu(j,2) 1-EandNu(j,2) 0; 0 0 (1-2*(EandNu(j,2)))/2];
     sigma(j,:)=C*Belement*u([P(elements(j,1),:) P(elements(j,2),:) P(elements(j,3),:)]);
-    sigma(abs(sigma)<0.00001)=0;
+    sigma(abs(sigma)<null)=0;
     end
 end
 if size(elements,2) == 4  %bilinear quadrilateral elements
@@ -252,7 +254,7 @@ if size(elements,2) == 4  %bilinear quadrilateral elements
             bder(o+4)=a(2);
         end
         Belement=[bder(1) 0 bder(2) 0 bder(3) 0 bder(4) 0; 0 bder(5) 0 bder(6) 0 bder(7) 0 bder(8); bder(5) bder(1) bder(6) bder(2) bder(7) bder(3) bder(8) bder(4) ];
-        C=(Eandv(j,1)/(1-(Eandv(j,2))^2))*[1 Eandv(j,2) 0; Eandv(j,2) 1 0; 0 0 (1-Eandv(j,2))/2];
+        C=(EandNu(j,1)/(1-(EandNu(j,2))^2))*[1 EandNu(j,2) 0; EandNu(j,2) 1 0; 0 0 (1-EandNu(j,2))/2];
         w=zeros(4,3);
         for t=1:4
             gauss=[0.57735 0.57735;-0.57735 0.57735;0.57735 -0.57735;-0.57735 -0.57735];
@@ -262,23 +264,24 @@ if size(elements,2) == 4  %bilinear quadrilateral elements
             w(t,:)=CB*u([P(elements(j,1),:) P(elements(j,2),:) P(elements(j,3),:) P(elements(j,4),:)]); %stress in each gauss point
         end
         sigma(j,:)=max(w); %find max stress between stresses in gauss points
-        sigma(abs(sigma)<0.00001)=0;
+        sigma(abs(sigma)<null)=0;
     end
 end
 toc;
 
+% Visualization of results for truss elements
 if size(elements,2) == 2  %truss elements
     disp('all of displacemnts and reaction forces of this problem:');
     
     %find coordinate of nodes after deformation for plot
-    v=Nodes_xy+100*reshape(u,2,[])';  %v is node coordinates after deformation
+    v=Nodes_xy+ScalingFactor*reshape(u,2,[])';  %v is node coordinates after deformation
     
-    nodes=zeros(2, no_nodes);
-    for a=1:no_nodes
+    nodes=zeros(2, n_nodes);
+    for a=1:n_nodes
         nodes(:,a)=a;
     end
     nodes=nodes(:);
-    orientation=repmat(['u';'v'],no_nodes,1);
+    orientation=repmat(['u';'v'],n_nodes,1);
  
     
     T=table(orientation,nodes,u,F)
@@ -302,21 +305,23 @@ if size(elements,2) == 2  %truss elements
         hold on
     end
     
-    legend([p1 p2],{'before','after'})
+    legend([p1 p2],{'before',"after (Scaling Factor = " + ScalingFactor + ")"})
  
 end
+
+% Visualization of results for triangle elements
 if size(elements,2) == 3  %triangle elements
     disp('all of displacemnts and stresses of this problem:(for CST element)');
     
     %find coordinate of nodes after deformation for plot
-    v=Nodes_xy+10*reshape(u,2,[])';  %v is node coordinates after deformation
+    v=Nodes_xy+ScalingFactor*reshape(u,2,[])';  %v is node coordinates after deformation
     
-    nodes=zeros(2, no_nodes);
-    for a=1:no_nodes
+    nodes=zeros(2, n_nodes);
+    for a=1:n_nodes
         nodes(:,a)=a;
     end
     nodes=nodes(:);
-    orientation=repmat(['u';'v'],no_nodes,1);
+    orientation=repmat(['u';'v'],n_nodes,1);
  
     
     T=table(orientation,nodes,u,F)
@@ -346,21 +351,23 @@ if size(elements,2) == 3  %triangle elements
         p6=plot(v([elements(s,1) elements(s,3)],1),v([elements(s,1) elements(s,3)],2),'o--','Color','red','linewidth',2,'markersize',4);
     end
     
-    legend([p1 p4],{'before','after'})
+    legend([p1 p4],{'before',"after (Scaling Factor = " + ScalingFactor + ")"})
  
 end
-if size(elements,2) == 4   %BILINEAR QUADRILATERAL elements
+
+% Visualization of results for bilinear quadrilateral elements
+if size(elements,2) == 4   %bilinear quadrilateral elements
     disp('all of displacemnts and stresses of this problem:(for BILINEAR QUADRILATERAL element)');
     
     %find coordinate of nodes after deformation for plot
-    v=Nodes_xy+100*reshape(u,2,[])';  %v is node coordinates after deformation
+    v=Nodes_xy+ScalingFactor*reshape(u,2,[])';  %v is node coordinates after deformation
     
-    nodes=zeros(2, no_nodes);
-    for a=1:no_nodes
+    nodes=zeros(2, n_nodes);
+    for a=1:n_nodes
         nodes(:,a)=a;
     end
     nodes=nodes(:);
-    orientation=repmat(['u';'v'],no_nodes,1);
+    orientation=repmat(['u';'v'],n_nodes,1);
  
     
     T=table(orientation,nodes,u,F)
@@ -394,9 +401,9 @@ if size(elements,2) == 4   %BILINEAR QUADRILATERAL elements
         p8=plot(v([elements(s,1) elements(s,4)],1),v([elements(s,1) elements(s,4)],2),'o--','Color','red','linewidth',2,'markersize',4);
     end
     
-    legend([p1 p5],{'before','after'})
+    legend([p1 p5],{'before',"after (Scaling Factor = " + ScalingFactor + ")"})
  
 end
 %--------------------------------------------------------------------------
-% End of post processing and showing outputs
+% End of Post-Processing and Visualization of Results
 %--------------------------------------------------------------------------
